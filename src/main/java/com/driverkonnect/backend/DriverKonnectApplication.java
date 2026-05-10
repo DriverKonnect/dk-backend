4d402fbc2e25e47aacd7ad79e4faa5b1206d2da2
radildev <radilrupasinghe@gmail.com>
2026-05-10 13:30:59 +0530

    chore(init): initial commit

---
+++ src/main/java/com/driverkonnect/backend/DriverKonnectApplication.java
@@ -0,0 +1,13 @@
+package com.driverkonnect.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DriverKonnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DriverKonnectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/driverkonnect/backend/DriverKonnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.driverkonnect.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DriverKonnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
