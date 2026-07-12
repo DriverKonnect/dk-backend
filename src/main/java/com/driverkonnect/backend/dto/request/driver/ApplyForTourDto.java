a17c2776081e8c77ba2c928f9bf7f756fc442970
Kavith-PredictivAI <kavith@predictiv.ai>
2026-07-12 21:01:25 +0530

    feat(TourManagement): add tour application functionality for drivers

---
+++ src/main/java/com/driverkonnect/backend/dto/request/driver/ApplyForTourDto.java
@@ -0,0 +1,12 @@
+package com.driverkonnect.backend.dto.request.driver;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ApplyForTourDto {
+    private String note;
+}
